923792cd5188023b0e57f4db98fbbcf3aaeba46d
Aashiya khan <aashiyakhan403@gmail.com>
2025-12-30 17:11:12 +0530

    revising

---
+++ revisionstrmethod.py
@@ -0,0 +1,49 @@
+# string methods 
+a = "harry"
+print(a.lower())
+
+a = "hello!"
+print(a.rstrip("!"))
+
+b = "hello im aashiya and he is priyanshu"
+print(b.replace("aashiya","maladevi"))
+
+c = "silver spoon is there"
+print(c.split(" "))
+
+d = "hey there"
+print(d.capitalize())
+
+e = "we are coding right now"
+print(e.center(40))
+
+f = "harry"
+print(f.count("r"))
+
+g = "we are practising!"
+print(g.endswith("are",1,6))
+
+
+h = "we will check and we will answer"
+print(h.index("we"))
+
+
+i= "the4password6is5Aashiya007"
+print(i.isalnum())
+
+j = "none"
+print(j.islower())
+
+k = "i am eating"
+print(k.capitalize())
+
+l = "Hello Guys I"
+print(l.istitle())
+
+m = "hello\niam aashiya\nyk"
+print(m.isprintable())
+
+
+
+
+
